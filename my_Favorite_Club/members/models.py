d21996ac6bcc31d370f6d042b80441962106f0c0
Odilbek <lezginodilbek@gmail.com>
2024-03-19 17:43:29 +0500

    gf

--- my_Favorite_Club/members/models.py
+++ my_Favorite_Club/members/models.py
@@ -3,4 +3,8 @@ from django.db import models
 class Member(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
-# Create your models here.
+  phone = models.CharField(max_length=255)
+  joined_date = models.DateField(null=True)
+  def __str__(self):
+    return f"{self.firstname} {self.lastname}"
+
